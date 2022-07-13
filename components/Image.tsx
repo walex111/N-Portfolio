68655c2f53ca81d82c2bb2aab341c583d496b5ac
Samson Oketunmbi <Oyattunechi@gmail.com>
2022-07-13 15:18:26 -0600

    added some components and installed framer motion

---
+++ components/Image.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+interface ImageProps {
+  src?: string;
+  alt?: string;
+  width: number;
+  height: number;
+  className?: string;
+}
+
+const ImageComp = ({
+  src = "",
+  alt = "",
+  width,
+  height,
+  className,
+}: ImageProps) => {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      className={className}
+    />
+  );
+};
+
+export default ImageComp;
---
+++ components/hamburgerMenu.tsx
@@ -0,0 +1,11 @@
+const HamburgerMenu = () => {
+  return (
+    <div className="space-y-2">
+      <span className="block w-5 h-0.5 bg-black"></span>
+      <span className="block w-8 h-0.5 bg-black"></span>
+      <span className="block w-8 h-0.5 bg-black"></span>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
