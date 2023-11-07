da28988dfdac08536edd2d9fb501d6fe100b3f7d
Samson Oketunmbi <samsonoketunmbi@Samsons-Mac-mini.local>
2023-11-07 12:02:07 -0700

    refurbished portfolio v2 error update custom favIcon

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,8 +45,28 @@ const Home: NextPage = ({}) => {
   return (
     <NoSSR>
       <Head>
-        <title>SAMSON OKETUNMBI</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title >Samson Oketunmbi</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <div className="h-screen w-full bg-darkc text-lightc text-opacity-70 relative">
