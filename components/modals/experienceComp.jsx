abd781192e2c66fc1611febc0f22bd4446282c58
walex111 <oyattunechi@gmail.com>
2025-03-16 15:24:40 -0600

    experience update

--- components/modals/experienceComp.jsx
+++ components/modals/experienceComp.jsx
@@ -11,38 +11,19 @@ const ExperienceComp = () => {
                         title: "Experience",
                         content: [
                             {
-                                duration: "SEP 2023 – Date",
-                                wTitle: "Full Stack Developer - Naver Corp",
-                                compantSite: "https://www.naver.com/",
-                                mContent: (
-                                    <>
-                                        <p>
-                                            Delivering high-quality, robust production code for a diverse
-                                            array of projects for clients while adopting best practices,nurturing professional growth and enhancing team efficacy in delivering top-tier software solutions. Also Optimizing server-side rendering in Next.js to improve SEO and accelerate first-page load times, while contributing to a cloud-based architecture using Docker and AWS to enhance scalability and resource management.
-                                        </p>
-                                    </>
-                                ),
-                                contentLang: [
-                                    "Next JS",
-                                    "Tailwind CSS",
-                                    "Javascript",
-                                    "Typescript",
-                                    "Node JS",
-                                    "AWS / Firebase",
-                                ],
-                            },
-                            {
-                                duration: "NOV 2021 – JUN 2023",
+                                duration: "NOV 2021– AUG 2024",
                                 wTitle: "Front-End Developer - Placewise Group",
                                 compantSite: "https://www.placewise.com/",
                                 mContent: (
                                     <>
                                         <p>
-                                            Deliver high-quality, robust production code for a diverse
-                                            array of projects for clients including Centennial
-                                            properties, and more. Providing camaraderie within
-                                            development department through close collaboration,
-                                            knowledge sharing.
+                                            I spearheaded the development and deployment of a suite of feature-rich Next.js applications, leveraging the power of TypeScript to create highly responsive and data-driven interfaces. These applications served as the core of a retail real-estate management platform, empowering users with enhanced data visualization capabilities. By meticulously optimizing the front-end architecture and data processing logic, I achieved a significant 20% improvement in data visualization efficiency, enabling users to gain deeper insights and make more informed decisions.
+
+                                            To ensure scalability and reliability, I integrated the applications with cloud computing environments on AWS, utilizing Docker and Kubernetes for containerization and deployment. This strategic approach facilitated seamless scaling during peak usage and streamlined the deployment process, resulting in a more robust and resilient system.
+
+                                            I prioritized the user experience by implementing responsive and visually appealing designs using Tailwind CSS, ensuring consistent branding and optimal performance across a wide range of devices. This commitment to front-end excellence contributed to a more engaging and intuitive user interface.
+
+                                            Demonstrating a proactive approach to continuous learning and adaptability, I expanded my technical skillset by delving into new technologies, including C++, Ruby, and PHP. This commitment to staying abreast of the latest advancements allowed me to contribute to diverse projects and tackle complex challenges with confidence.
                                         </p>
                                     </>
                                 ),
@@ -61,13 +42,9 @@ const ExperienceComp = () => {
                                 mContent: (
                                     <>
                                         <p>
-                                            Developed, maintained, and shipped production code of a
-                                            start-up e-commerce websites, employing a wide range of
-                                            modern front-end technologies and practices i.e A/B
-                                            testing and Image Optimization to deliver engaging user
-                                            experiences, streamline navigation, and optimize
-                                            performance focusing on 100% code re-use +
-                                            compositionFlexible tree component.
+                                            I spearheaded the development of feature-rich Next.js applications, leveraging TypeScript to create data-driven interfaces for a retail real-estate management platform. By optimizing front-end architecture and data processing, I achieved a 20% improvement in data visualization efficiency. Deployment on AWS, utilizing Docker and Kubernetes, ensured scalability and reliability. Collaborating closely with UX/UI designers, I translated Figma mocks into pixel-perfect, accessible interfaces, while Tailwind CSS provided consistent, responsive design. Redux integration optimized state management, and proactive performance tuning led to a 20% reduction in page load times and enhanced SEO.
+
+                                            To streamline development and maintain consistency, I developed and maintained reusable React components and services, resulting in a 30% reduction in development time. I also demonstrated continuous learning and adaptability by expanding my technical skillset to include C++, Ruby, and PHP. This comprehensive approach, encompassing both technical execution and strategic optimization, contributed to the successful delivery of a robust and user-friendly platform.
                                         </p>
                                     </>
                                 ),
@@ -79,28 +56,6 @@ const ExperienceComp = () => {
                                     "CSS",
                                     "SQL",
                                     "Firebase",
-                                ],
-                            },
-                            {
-                                duration: "FEB 2018 – FEB 2019",
-                                wTitle: "Web Developer Internship - Naver Corp",
-                                compantSite: "https://www.naver.com/",
-                                mContent: (
-                                    <>
-                                        <p>
-                                            Actively participated in agile development cycles, contributing to a collaborative learning environment while engaging in rigorous code review and adopted best practices, nurturing professional growth
-                                            while enhancing team efficacy in delivering top-tier software solutions.
-                                        </p>
-                                    </>
-                                ),
-                                contentLang: [
-                                    "HTML",
-                                    "CSS",
-                                    "Javascript",
-                                    "Typescript",
-                                    "Node js",
-                                    "Express",
-                                    "Python",
                                 ],
                             },
 
